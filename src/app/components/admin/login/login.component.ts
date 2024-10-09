6a152d292c02170823c7c1580e0f03573b414d51
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-10-09 15:10:35 +0300

    updates

--- src/app/components/admin/login/login.component.ts
+++ src/app/components/admin/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent {
 
       (err) =>
         this.s.fire({
-          title: err.error,
+          title: "invalid email or password",
           icon: 'error',
         })
     );
