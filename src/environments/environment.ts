a40459f28872c061c0f28dab2973aaf2cfb4ca5e
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-11-09 10:04:19 +0200

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
   production: true,
-  baseAPIURL: 'http://localhost:3344',
+  baseAPIURL: 'https://mostaqel-clone-back-end-abdelrahmans-projects-44ce2102.vercel.app',
   paypalClientId: 'AQ2wsW3SjeRbCCLbYwsOfpOKy5uFrlvx6rin8R7dEA9_upmQ7jWIeD0Xu-9DDnXn8zlfyevnmUqLP3qm'
 
 };
