34460863831f02a6a98c2caa39a008e1a2b50df7
abdelrahman-elsaady <abdelrahmanelnobby@gmail.com>
2025-05-13 19:50:29 +0300

    4

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseAPIURL: 'https://mostaqel-clone-back-end-abdelrahmans-projects-44ce2102.vercel.app',
+  paypalClientId: 'AQ2wsW3SjeRbCCLbYwsOfpOKy5uFrlvx6rin8R7dEA9_upmQ7jWIeD0Xu-9DDnXn8zlfyevnmUqLP3qm'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+
+
+export const environment = {
+  production: true,
+  baseAPIURL: 'https://mostaqel-clone-back-end-abdelrahmans-projects-44ce2102.vercel.app',
+  paypalClientId: 'AQ2wsW3SjeRbCCLbYwsOfpOKy5uFrlvx6rin8R7dEA9_upmQ7jWIeD0Xu-9DDnXn8zlfyevnmUqLP3qm'
+
+};
